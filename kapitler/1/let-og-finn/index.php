32fa5c896bdc251fb9e2f6b5ffd618f3ad83ef8f
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-04-03 04:14:23 +0200

    legg til bakgrunnsbilde i fortelling

---
+++ kapitler/1/let-og-finn/index.php
@@ -0,0 +1,43 @@
+<?php
+include '../../../inc/functions.php';
+$title = "1 - Let og finn";
+$depth = 3;
+$h1 = $title;
+$pageClass = "kort kap-1";
+?>
+<?php theHeader($depth, $title, $h1, $pageClass); ?>
+
+<div class="row">
+	<div class="col-xd-10 col-xs-10">
+
+    <h2>Hvor mange ord finner du?</h2>
+
+    <!--<ol>
+        <li>romskip</li>
+        <li>humle</li>
+        <li>katt</li>
+        <li>krokus</li>
+        <li>selje</li>
+        <li>bjørk</li>
+        <li>Samir</li>
+        <li>Jenny</li>
+        <li>sukkererter</li>
+    </ol>-->
+    	<div class="row">
+    	   <div class="col-xs-12 col-xd-12">
+                <object type="image/svg+xml" data="<?php theRoot($depth); ?>media/interactive/oversiktsbilde-kapittel01.svg">
+                    <p>Your browser does not support SVG</p>p>
+                </object>
+           </div><!-- .col -->
+        </div><!-- div.row -->
+
+	</div><!-- .col-->
+
+	<div class="col-xd-2 col-xs-2">
+		<?php theChapterMenu(0); ?>
+	</div><!-- .col-xd-4  -->
+</div><!-- .row -->
+
+
+
+<?php theFooter($depth); ?>
